56f0a2d7c9159df0e34e364205cfd474eb0c503f
Evelynn Doan <kdoa8440@uni.sydney.edu.au>
2025-06-04 15:46:01 +1000

    Fix propswithchildren<{}> to <unknown> for vercel build

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "react";
 
-export const PageLayout = (props: PropsWithChildren<{}>) => {
+export const PageLayout = (props: PropsWithChildren<unknown>) => {
     return(
         <main className="flex h-screen justify-center">
             <div className="h-full w-full md:max-w-2xl bg-black border-x border-slate-400 overflow-y-scroll">
